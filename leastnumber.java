fa75e08f05d70cf68f8a0e93690821a575eee17e
soundharya1029 <mvsoundharya97@gmail.com>
2017-06-14 12:20:51 +0530

    Create leastnumber.java

--- leastnumber.java
+++ leastnumber.java
@@ -1,22 +1,9 @@
 import java.io.*;
-import java.util.*;
 import java.lang.*;
+import java.util.*;
 public class leastnumber
 {
-public static void main(String args[])
-{
-int c=0;
-Scanner sc=new Scanner(System.in);
-int n=sc.nextInt();
-String s=sc.nextLine();
-char ch[]=s.toCharArray();
-Arrays.sort(ch);
-for(int i=0;i<ch.length-n;i++)
-{
-ch1[c]=ch[i];
-c++;
-}
-String ans=String.valueOf(ch1);
-System.out.println(ans);
-}
-}
+  public static void main(String args[])
+  {
+    
+    
