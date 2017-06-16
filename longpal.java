c93c6c0868fd5aa2d25e08346d8c77b6448d5368
soundharya1029 <mvsoundharya97@gmail.com>
2017-06-16 12:20:11 +0530

    Create longpal.java

---
+++ longpal.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.lang.*;
+public class longpal
+{
+    public static void main(String args[])
+    {
+        int i,j,c=0,k;
+        String pal="";
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char ch[]=s.toCharArray();
+        String ans[]=new String[50];
+        for(i=0;i<ch.length;i++)
+        {
+            for(j=ch.length-1;j>=0;j--)
+            {
+                if(i<j)
+                {
+                if(ch[i]==ch[j])
+                {
+                    String str=s.substring(i,j+1);
+                    StringBuffer sb=new StringBuffer(str);
+                    String str2=sb.reverse().toString();
+                    if(str2.equals(str))
+                    { 
+                      ans[c]=str;
+                      c++;
+                    }
+                }
+                } 
+            }
+        }
+       int mlen=ans[0].length();
+       for(k=1;k<c;k++)
+       {
+           int clen=ans[k].length();
+           if(clen>=mlen)
+           {
+               mlen=clen;
+               pal=ans[k];
+           }
+       }
+       System.out.println(pal);
+    }
+}
