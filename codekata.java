09ebb5bba77b35a6cc132059e6a698687261cea1
soundharya1029 <mvsoundharya97@gmail.com>
2017-06-21 14:21:39 +0530

    Update codekata.java

--- codekata.java
+++ codekata.java
@@ -4,7 +4,6 @@ public class codekata
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter two strings:");
         String s1=sc.nextLine();
         String s2=sc.nextLine();
         String s1l=s1.toLowerCase();
