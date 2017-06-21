aa6581411f2e19624719eb9d5229debddfdc9ce1
soundharya1029 <mvsoundharya97@gmail.com>
2017-06-21 10:26:30 +0530

    Create elephant.java

---
+++ elephant.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class elephant
+{
+    public static void main(String args[])
+    {
+        int sum=0,count=0;
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int w=sc.nextInt();
+        int ch[]=new int[m];
+        for(int i=0;i<m;i++)
+        {
+            ch[i]=sc.nextInt();
+        }
+        for(int j=m-1;j>=0;j--)
+        {
+            if(sum<w)
+            {
+               sum=sum+ch[j];
+               count++;
+            }
+            else if(sum==w)
+               break;
+            else if(sum>w)
+            {
+               sum=sum-ch[j+1];
+               count--;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
